4c502407df2fec6861541514895c0cd1f8bf901a
LazorykV <lazorkinv@gmail.com>
2024-03-02 19:07:46 +0200

    Add 'City' column and 'Delete' button in UsersPage
    A new 'City' column has been added to the data grid in the UsersPage which displays the city information from a user's address. Also, a 'Delete' button is introduced under the 'Action' column, providing an additional functionality to the user interface. Enhancements in styling and indentation for improved code readability were also made.

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,64 +1,77 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { usersRequest, usersSuccess, usersFailure } from '../features/userSlice';
-import { DataGrid } from '@mui/x-data-grid';
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {usersRequest, usersSuccess, usersFailure} from '../features/userSlice';
+import {DataGrid} from '@mui/x-data-grid';
 import {userAPI} from "../api/userAPI.jsx";
 import Button from '@mui/material/Button';
 
 export const UsersPage = () => {
-  const dispatch = useDispatch();
-  const stateSlice = useSelector((state) => state.users);
+    const dispatch = useDispatch();
+    const stateSlice = useSelector((state) => state.users);
 
-  useEffect(() => {
-    dispatch(usersRequest());
+    useEffect(() => {
+        dispatch(usersRequest());
 
-    userAPI.getUsers()
-      .then((usersData) => {
-        dispatch(usersSuccess(usersData));
-      })
-      .catch((error) => {
-        dispatch(usersFailure(error.toString()));
-      });
-  }, [dispatch]);
+        userAPI.getUsers()
+            .then((usersData) => {
+                dispatch(usersSuccess(usersData));
+            })
+            .catch((error) => {
+                dispatch(usersFailure(error.toString()));
+            });
+    }, [dispatch]);
 
-  if (stateSlice.loading) {
-    return <div>Loading...</div>;
-  }
+    if (stateSlice.loading) {
+        return <div>Loading...</div>;
+    }
 
-  if (stateSlice.error) {
-    return <div>Error: {stateSlice.error}</div>;
-  }
+    if (stateSlice.error) {
+        return <div>Error: {stateSlice.error}</div>;
+    }
 
-  const columns = [
-    { field: 'username', headerName: 'Username', flex: 1 },
-    { field: 'phone', headerName: 'Phone', flex: 1 },
-    { field: 'website', headerName: 'Website', flex: 1 },
-    {
-      field: 'edit',
-      headerName: 'Action',
-      sortable: false,
-      flex: 0.5,
-      renderCell: () => (
-          <Button
-            variant="contained"
-            color="primary"
-            size="small"
-          >
-            Edit
-          </Button>
-      ),
-    },
-  ];
+    const columns = [
+        {field: 'username', headerName: 'Username', flex: 1},
+        {field: 'phone', headerName: 'Phone', flex: 1},
+        {field: 'website', headerName: 'Website', flex: 1},
+        {
+            field: 'city',
+            headerName: 'City',
+            flex: 1,
+            valueGetter: (params) => params.row.address?.city
+        },
+        {
+            field: 'edit',
+            headerName: 'Action',
+            sortable: false,
+            flex: 0.8,
+            renderCell: () => (
+                <div>
+                  <Button
+                      variant="contained"
+                      color="primary"
+                      size="small"
+                      style={{marginRight: '6px'}}
+                  >
+                    Edit
+                  </Button>
+                  <Button variant="contained" color="secondary" size="small">
+                    Delete
+                  </Button>
+                </div>
+
+            ),
+        },
+    ];
 
   return (
-    <div style={{ height: 400, width: '70vw' }}>
-      <DataGrid
-        rows={stateSlice.users}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        getRowId={(row) => row.id}
-      />
-    </div>
-  );
+      <div style={{height: '80vh', width: '70vw'}}>
+        <DataGrid
+            rows={stateSlice.users}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            getRowId={(row) => row.id}
+            />
+        </div>
+    );
 };
